23fa2adf9eeaaacf4d29d77cc8214b1e7226428f
Ingy döt Net <ingy@ingy.net>
2014-08-06 13:25:05 -0700

    CPAN Release 0.73
    - Doc fix. Thanks @Perlover++

--- lib/Module/Install/TestBase.pm
+++ lib/Module/Install/TestBase.pm
@@ -6,7 +6,7 @@ use Module::Install::Base;
 
 use vars qw($VERSION @ISA);
 BEGIN {
-    $VERSION = '0.72';
+    $VERSION = '0.73';
     @ISA     = 'Module::Install::Base';
 }
 
--- lib/Test/Base.pm
+++ lib/Test/Base.pm
@@ -1,5 +1,5 @@
 package Test::Base;
-our $VERSION = '0.72';
+our $VERSION = '0.73';
 
 use Spiffy -Base;
 use Spiffy ':XXX';
